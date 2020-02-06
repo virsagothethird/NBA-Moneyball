6aebdd5d4e94bea328019e37b6e38473280a64d3
Joo <jookim.jkk@gmail.com>
2020-02-06 13:52:17 -0600

    made helper.py

---
+++ src/helpers.py
@@ -0,0 +1,42 @@
+import pandas as pd
+import numpy as np
+
+
+def df_year_split(df, year, drop_columns):
+    
+    return df[df['Year']==year].drop(columns=drop_columns)\
+                            .drop_duplicates('Player')\
+                            .reset_index().drop(columns='index')
+
+
+def time_series_me(df1,df2,df3,df_for_y):
+    
+    time_series_df = df1.copy()
+    
+    time_series_df = df1.set_index('Player')\
+                        .join(df2.set_index('Player'),lsuffix='_1', rsuffix='_2',how='outer')\
+                        .join(df3.set_index('Player'),lsuffix='_2', rsuffix='_3',how='outer')\
+                        .join(df_for_y.set_index('Player'),lsuffix='_3', rsuffix='_4',how='outer')\
+                        .reset_index()
+    
+    time_series_df = time_series_df[time_series_df['MP_4']>200].dropna(subset=['TRB_3', 'TRB_4'])
+    time_series_df = time_series_df.fillna(0)
+    #time_series_df = time_series_df[time_series_df['MP_4']>200].dropna
+
+    columns_to_drop = ['Player', 'Pos_1', 'Tm_1', 'Pos_2',
+       'Tm_2', 'Pos_3', 'Tm_3',
+       'Pos_4', 'Age_4', 'Tm_4', 'G_4', 'MP_4',
+       'WS_4', 'WS/48_4', 'FG_4', 'FGA_4', '3P_4', '3PA_4', '2P_4', '2PA_4',
+       'FT_4', 'FTA_4', 'TRB_4', 'AST_4', 'STL_4', 'BLK_4', 'TOV_4', 'PF_4',
+       'PTS_4']
+    X = time_series_df.drop(columns=columns_to_drop)
+    y = time_series_df['WS/48_4']
+    
+    return time_series_df,X,y
+
+
+def residual_plot(ax, x, y, y_hat, n_bins=50):
+    residuals = y_hat - y
+    ax.axhline(0, color="black", linestyle="--")
+    ax.scatter(x, residuals, color="grey", alpha=0.5)
+    ax.set_ylabel("Residuals ($\hat y - y$)")
